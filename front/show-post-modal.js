3270bf2d72808ca916109764fce7735e8fc19c44
busaikuna <kunaabus@gmail.com>
2024-09-07 09:47:14 -0300

    correction

--- front/show-post-modal.js
+++ front/show-post-modal.js
@@ -1,11 +1,6 @@
 const modal = document.querySelector("dialog");
 const modalPic = document.querySelector("#change-pic")
 const addButton = document.querySelector("#add");
-const inputImage = document.querySelector("#account-image")
-
-inputImage.addEventListener("change", ()=>{
-   modalPic.showModal()
-})
 
 addButton.addEventListener('click', () => {
    modal.showModal()
